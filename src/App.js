b2e7e83a23b7826f9571ce6ad3081ee0c69f5a58
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-08-24 17:48:07 +0500

    CrudDone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { users } from './mock';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      status: "",
+      datalist: users
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleCreate = () => {
+    const { name, status, datalist } = this.state;
+    const newUser = { id: datalist.length + 1, name, status };
+    this.setState({ datalist: [...datalist, newUser], name: "", status: "" });
+  };
+
+  handleDelete = (id) => {
+    const { datalist } = this.state;
+    const updatedList = datalist.filter((user) => user.id !== id);
+    this.setState({ datalist: updatedList });
+  };
+
+  handleEdit = (id) => {
+    const { datalist } = this.state;
+    const user = datalist.find((user) => user.id === id);
+    if (user) {
+      this.setState({ name: user.name, status: user.status });
+    }
+  };
+
+  handleUpdate = (id) => {
+    const { name, status, datalist } = this.state;
+    const updatedList = datalist.map((user) => {
+      if (user.id === id) {
+        return { ...user, name, status };
+      }
+      return user;
+    });
+    this.setState({ datalist: updatedList, name: "", status: "" });
+  };
+
+  render() {
+    const { name, status, datalist } = this.state;
+
+    return (
+      <div>
+        <h1>User List</h1>
+        <table border={1} width={600}> 
+          <thead>
+            <tr >
+              <th>ID</th>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {datalist.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.status}</td>
+                <td>
+                  <button onClick={() => this.handleDelete(user.id)}>Delete</button>
+                  <button onClick={() => this.handleEdit(user.id)}>Edit</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <h2>Add User</h2>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={this.handleInputChange}
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          name="status"
+          value={status}
+          onChange={this.handleInputChange}
+          placeholder="Status"
+        />
+        <button onClick={this.handleCreate}>Create</button>
+        <button onClick={() => this.handleUpdate(users.id)}>Update</button>
+      </div>
+    );
+  }
 }
---
+++ src/mock.js
@@ -0,0 +1,6 @@
+export const users = [
+    { id: 1, name: 'Ashrafxon', status: 'Student' },
+    { id: 2, name: 'Bobur', status: 'Teacher' },
+    { id: 3, name: 'Nodir', status: 'Pilot' },
+    { id : 4, name: 'Maxmud', status: 'Driver' }
+];
